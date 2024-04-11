06add13d2a3f1b3bb6f5582fc9c033d6456efb33
Uzair Sharif <Uzair.Sharif@smartmicro.de>
2024-04-11 21:10:38 +0200

    [WIP] feat: Supports more features and improves code quality

--- main.cpp
+++ main.cpp
@@ -27,17 +27,21 @@
 #include <future>
 #include <iostream>
 #include <queue>
+#include <expected>
 
 #include <nlohmann/json.hpp>
 #include <spdlog/spdlog.h>
 
 // TODO: possibly make everything noexcept for faster code
 
+// TODO: concept for PasswordsMap template type
+// TODO: protected access to PasswordsMap
+
 namespace {
 
 std::string_view constexpr kPasswordFilePath{"/tmp/passwords.json"};
 
-enum class UserEvent { kKill, kNone };
+enum class UserEvent { kAdd, kKill, kNone };
 
 template <class Element>
 class ThreadSafeQueue final {
@@ -80,14 +84,14 @@ concept CommunicationChannelConcept = requires(CommunicationChannel channel, Mes
 };
 
 /// Loads current stored passwords from disk to memory for faster in-app access
-auto LoadPasswords() {
+template <class PasswordsMap>
+auto LoadPasswords() -> std::expected<PasswordsMap, std::string> {
   // NOTE: This might seem to be a unnecessary check but this routine is meant to be called rarely (e.g. at start of program) so this check can still be kept as
   // it will not have an performance impact.
   if (not std::filesystem::exists(kPasswordFilePath)) {
     std::ofstream password_f{kPasswordFilePath};
     if (not password_f.is_open()) {
-      spdlog::error("Could not create an empty passwords file on specified path.");
-      return;
+      return std::unexpected{"Could not create an empty passwords file on specified path."};
     }
 
     password_f << R"({"foo": "bar"})" << '\n';
@@ -98,66 +102,104 @@ auto LoadPasswords() {
   try {
     password_f >> password_j;
   } catch (std::exception const& e) {
-    spdlog::error("Passwords file seems corrupted as couldn't parse into json format. Exception: {}", e.what());
-    return;
+    return std::unexpected{std::format("Passwords file seems corrupted as couldn't parse into json format. Exception: {}", e.what())};
   }
+
+  PasswordsMap passwords_map{};
+  std::ranges::transform(password_j.items(), std::inserter(passwords_map, std::end(passwords_map)),
+                         [](auto const& entry) { return std::make_pair(entry.key(), entry.value()); });
+  return passwords_map;
 }
 
 auto WelcomeScreen() {
   std::println("Welcome to Password Manager!!");
+  std::println("a : add password entry");
   std::println("x : close app");
+  std::println("S : show all managed passwords");
   std::println("");
 }
 
-template <CommunicationChannelConcept CommunicationChannel>
-auto UserInputTask(std::shared_ptr<CommunicationChannel> communication_channel_arg) {
-  auto communication_channel{communication_channel_arg};
+template <CommunicationChannelConcept CommunicationChannel, class PasswordsMap>
+auto UserInputTask(std::shared_ptr<CommunicationChannel> communication_channel, std::shared_ptr<PasswordsMap> passwords_map) {
+  using namespace std::literals;
 
-  while (true) {
-    std::string input{};
+  std::string input{};
+  bool keep_going{true};
+
+  std::unordered_map<std::string_view, std::function<void()>> const kActionMap{
+      {"x"sv,
+       [&communication_channel, &keep_going]() {
+         communication_channel->Push(Message{.user_event = UserEvent::kKill});
+         keep_going = false;
+       }},
+      {"a"sv, [&communication_channel]() { communication_channel->Push(Message{.user_event = UserEvent::kAdd}); }},
+      {"S"sv, [&passwords_map]() {
+         std::println("Currently, I am managing following passwords:");
+         for (auto const& [k, v] : *passwords_map) {
+           std::println("{} -> {}", k, v);
+         }
+       }}};
+
+  while (keep_going) {
     std::getline(std::cin, input);
 
-    if (input == "x") {
-      communication_channel->Push(Message{.user_event = UserEvent::kKill});
-      break;
+    try {
+      kActionMap.at(input)();
+    } catch (std::exception const& ex) {
+      std::println("The passed input is not supported yet. Please try again!");
+      std::println("");
     }
   }
 }
 
-template <CommunicationChannelConcept CommunicationChannel>
-auto UserEventHandler(std::shared_ptr<CommunicationChannel> communication_channel_arg) {
+// TODO: why not pass shared_ptr as template type -> possibly because the concept applies to resource type and not shared_ptr!!
+template <CommunicationChannelConcept CommunicationChannel, class PasswordsMap>
+auto UserEventHandler(std::shared_ptr<CommunicationChannel> communication_channel, std::shared_ptr<PasswordsMap> passwords_map) {
   using namespace std::chrono_literals;
 
-  auto communication_channel{communication_channel_arg};
-
   bool keep_going{true};
+
+  std::unordered_map<UserEvent, std::function<void()>> const kActionMap{{UserEvent::kKill, [&keep_going]() { keep_going = false; }},
+                                                                        {UserEvent::kAdd, []() {
+                                                                           std::print("username: ");
+                                                                           std::string username{};
+                                                                           std::getline(std::cin, username);
+
+                                                                           std::print("password: ");
+                                                                           std::string password{};
+                                                                           std::getline(std::cin, password);
+                                                                         }}};
+
   while (keep_going) {
     // TODO: use condition-variable instead for more precise timing control
     std::this_thread::sleep_for(10ms);
 
     while (not communication_channel->Empty()) {
       auto message{communication_channel->Pop()};
-      switch (message.user_event) {
-        case UserEvent::kKill: {
-          keep_going = false;
-          break;
-        }
-        case UserEvent::kNone: {
-          spdlog::error("Queue got corrupted somehow as it got a kNone user-event.");
-          break;
-        }
+
+      try {
+        kActionMap.at(message.user_event)();
+      } catch (std::exception const& ex) {
+        spdlog::error("No action associated with {} user-event.", static_cast<int>(message.user_event));
       }
     }
   }
 }
 
-auto Initialize() {
-  WelcomeScreen();
-  LoadPasswords();
+auto SavePasswords() {
+  // TODO: save passwords
 }
 
-template <CommunicationChannelConcept CommunicationChannel>
+template <CommunicationChannelConcept CommunicationChannel, class PasswordsMap>
 auto LaunchTasks() {
+  auto expected_passwords_map{LoadPasswords<PasswordsMap>()};
+  if (not expected_passwords_map.has_value()) {
+    spdlog::error(expected_passwords_map.error());
+    return;
+  }
+
+  auto passwords_map{std::make_shared<PasswordsMap>(expected_passwords_map.value())};
+
   // set up the communication channel meant for inter-thread communication
   auto communication_channel{std::make_shared<CommunicationChannel>()};
 
@@ -165,24 +207,18 @@ auto LaunchTasks() {
   // - one handling user input
   // - one reacting to user events
 
-  auto ui_fut{std::async(std::launch::async, UserInputTask<CommunicationChannel>, communication_channel)};
+  auto ui_fut{std::async(std::launch::async, UserInputTask<CommunicationChannel, PasswordsMap>, communication_channel, passwords_map)};
+  auto eh_fut{std::async(UserEventHandler<CommunicationChannel, PasswordsMap>, communication_channel, passwords_map)};
+
   ui_fut.wait();
-
-  auto eh_fut{std::async(std::launch::async, UserEventHandler<CommunicationChannel>, communication_channel)};
   eh_fut.wait();
-}
 
-auto Cleanup() {
-  // TODO: save passwords
+  SavePasswords();
 }
 
 }  // namespace
 
 auto main() -> int {
-  // get app into a valid start up state
-  Initialize();
-
-  LaunchTasks<MessageQueue>();
-
-  Cleanup();
+  WelcomeScreen();
+  LaunchTasks<MessageQueue, std::unordered_map<std::string, std::string>>();
 }
