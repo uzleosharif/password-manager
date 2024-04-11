f1c76bbfae791bad8c71258f4ce5318464f5ba58
Uzair Sharif <Uzair.Sharif@smartmicro.de>
2024-04-11 11:43:39 +0200

    feat: Gracefully exits the application on user request

--- main.cpp
+++ main.cpp
@@ -5,11 +5,21 @@
 // -----------
 // {account -> password} stored as encrypted blob on disk
 
+// NOTE:
+// For inter-thread communication, there are generally 2 possibilities:
+// - Message passing
+//   - Producer thread generates the message and passes it over to consumer thread
+// - Message queue
+//   - Prodcuer keeps generating events and pushing to the queue while consumer keeps working on them
+//
+// Due to non-blocking nature of message-queue mechanism, we have opted to go for message-queue strategy.
+
 #include <fstream>
 #include <string_view>
 #include <print>
 #include <future>
 #include <iostream>
+#include <queue>
 
 #include <nlohmann/json.hpp>
 #include <spdlog/spdlog.h>
@@ -19,6 +29,48 @@
 namespace {
 
 std::string_view constexpr kPasswordFilePath{"/tmp/passwords.json"};
+
+enum class UserEvent { kKill, kNone };
+
+template <class Element>
+class ThreadSafeQueue final {
+ public:
+  auto Push(Element&& element) {
+    std::lock_guard<std::mutex> lock{m_mutex};
+    m_queue.push(std::forward<Element>(element));
+  }
+
+  auto Pop() {
+    std::lock_guard<std::mutex> lock{m_mutex};
+    auto element{m_queue.front()};
+    m_queue.pop();
+    return element;
+  }
+
+  auto Empty() {
+    std::lock_guard<std::mutex> lock{m_mutex};
+    return m_queue.empty();
+  }
+
+  auto Print() { std::println("this is thread-safe-queue"); }
+
+ private:
+  std::queue<Element> m_queue{};
+  std::mutex m_mutex{};
+};
+
+struct Message {
+  UserEvent user_event{UserEvent::kNone};
+};
+
+using MessageQueue = ThreadSafeQueue<Message>;
+
+template <class CommunicationChannel>
+concept CommunicationChannelConcept = requires(CommunicationChannel channel, Message&& message) {
+  { channel.Push(std::move(message)) } -> std::same_as<void>;
+  { channel.Pop() } -> std::same_as<Message>;
+  { channel.Empty() } -> std::same_as<bool>;
+};
 
 /// Loads current stored passwords from disk to memory for faster in-app access
 auto LoadPasswords() {
@@ -42,28 +94,82 @@ auto LoadPasswords() {
     spdlog::error("Passwords file seems corrupted as couldn't parse into json format. Exception: {}", e.what());
     return;
   }
-  std::println("{}", password_j.dump());
 }
 
-auto UserInputTask() {
+auto WelcomeScreen() {
+  std::println("Welcome to Password Manager!!");
+  std::println("x : close app");
+  std::println("");
+}
+
+template <CommunicationChannelConcept CommunicationChannel>
+auto UserInputTask(std::shared_ptr<CommunicationChannel> communication_channel_arg) {
+  auto communication_channel{communication_channel_arg};
+
   while (true) {
     std::string input{};
     std::getline(std::cin, input);
 
-    std::println("Processing user input: {}", input);
+    if (input == "x") {
+      communication_channel->Push(Message{.user_event = UserEvent::kKill});
+      break;
+    }
   }
 }
 
-}  // namespace
+template <CommunicationChannelConcept CommunicationChannel>
+auto UserEventHandler(std::shared_ptr<CommunicationChannel> communication_channel_arg) {
+  using namespace std::chrono_literals;
 
-auto main() -> int {
-  // load the current passowrds (json) file into memory
+  auto communication_channel{communication_channel_arg};
+
+  bool keep_going{true};
+  while (keep_going) {
+    // TODO: use condition-variable instead for more precise timing control
+    std::this_thread::sleep_for(10ms);
+
+    while (not communication_channel->Empty()) {
+      auto message{communication_channel->Pop()};
+      switch (message.user_event) {
+        case UserEvent::kKill: {
+          keep_going = false;
+          break;
+        }
+        case UserEvent::kNone: {
+          spdlog::error("Queue got corrupted somehow as it got a kNone user-event.");
+          break;
+        }
+      }
+    }
+  }
+}
+
+auto Initialize() {
+  WelcomeScreen();
   LoadPasswords();
+}
+
+template <CommunicationChannelConcept CommunicationChannel>
+auto LaunchTasks() {
+  // set up the communication channel meant for inter-thread communication
+  auto communication_channel{std::make_shared<CommunicationChannel>()};
 
   // set up async tasks:
   // - one handling user input
   // - one reacting to user events
 
-  auto user_input_task_f{std::async(std::launch::async, UserInputTask)};
-  user_input_task_f.wait();
+  auto ui_fut{std::async(std::launch::async, UserInputTask<CommunicationChannel>, communication_channel)};
+  ui_fut.wait();
+
+  auto eh_fut{std::async(std::launch::async, UserEventHandler<CommunicationChannel>, communication_channel)};
+  eh_fut.wait();
+}
+
+}  // namespace
+
+auto main() -> int {
+  // get app into a valid start up state
+  Initialize();
+
+  LaunchTasks<MessageQueue>();
 }
